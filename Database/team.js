4a807593413357940c1f89568edc252ecb532ea7
Cirlorm <akotosel6@gmail.com>
2019-04-03 08:12:13 +0000

    font change

--- Database/team.js
+++ Database/team.js
@@ -2,7 +2,7 @@ export const ourTeams = [
   {
     twitter: "@cbavlon",
     teamName: "Akoto Jonathan Selorm",
-    about: "Javascript Lover, Bloger, Music Addict.",
+    about: "Javascript Lover, Blogger, Music Addict.",
     teamURL: "https://i.ibb.co/nBPrRvT/cirlorm.png",
     githubUser: "https://github.com/champ3oy/",
     twitterUser: "https://twitter.com/cbavlon"
